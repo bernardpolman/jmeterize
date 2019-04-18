6bd118344252d753beec2b6af6b940dd2f51ad6e
Bernard Polman <polman.et2@gmail.com>
2019-04-18 12:45:34 +0200

    First commit

---
+++ JMeterizeApp/HelperMethods.cs
@@ -0,0 +1,70 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Forms;
+using System.Windows.Media;
+
+namespace JMeteorApp
+{
+    class HelperMethods
+    {
+        public static void WriteToLogWindow(TextBlock t, string message, int messageType = 0)
+        {
+            SolidColorBrush color = new SolidColorBrush();
+
+            switch (messageType)
+            {
+                case 0:
+                    color = Brushes.Black;
+                    break;
+                case 1:
+                    color = Brushes.ForestGreen;
+                    break;
+                case 2:
+                    color = Brushes.Red;
+                    break;
+                case 3:
+                    color = Brushes.Orange;
+                    break;
+            }
+
+            Run logLine = new Run(message + "\r\n");
+            logLine.Foreground = color;
+            t.Inlines.Add(logLine);
+        }
+
+        public static string CheckInstallationPath()
+        {
+            string folderPath = JMeterizeApp.Properties.Settings.Default.JMeterDirectory;
+
+            if(folderPath == string.Empty || !File.Exists(folderPath + Path.DirectorySeparatorChar + "jmeter.bat"))
+            {
+                System.Windows.MessageBox.Show("Please locate your 'bin' folder inside your JMeter software location (JMeter v.1.7+ required)", "JMeterize");
+                FolderBrowserDialog directory = new FolderBrowserDialog();
+
+                using (directory)
+                {
+                    if (directory.ShowDialog() == DialogResult.OK)  //check for OK...they might press cancel, so don't do anything if they did.
+                    {
+                        string test = directory.SelectedPath + Path.DirectorySeparatorChar + "jmeter.bat";
+
+                        if (!File.Exists(directory.SelectedPath + Path.DirectorySeparatorChar + "jmeter.bat"))
+                        {
+                            System.Windows.MessageBox.Show("File 'jmeter.bat' was not found. Please verify that your JMeter folder is not missing any files!", "Alert", MessageBoxButton.OK, MessageBoxImage.Warning);
+                        }
+                        else
+                        {
+                            folderPath = directory.SelectedPath;
+                            JMeterizeApp.Properties.Settings.Default.JMeterDirectory = directory.SelectedPath;
+                            JMeterizeApp.Properties.Settings.Default.Save();
+                        }
+                    }
+                }
+            }
+
+            return folderPath;
+        }
+    }
+}
---
+++ JMeterizeApp/MainWindow.xaml.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using static JMeteorApp.HelperMethods;
+
+namespace JMeteorApp
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public bool FileOk { get; set; }
+
+        public string scriptFullPath = string.Empty;
+        public string scriptDirectoryPath = string.Empty;
+        public string scriptFilename = string.Empty;
+        public string installationDirectory = string.Empty;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            StyleProperty.OverrideMetadata(typeof(Window), new FrameworkPropertyMetadata
+            {
+                DefaultValue = FindResource(typeof(Window))
+            });
+
+            FileOk = false;
+            var process = new Process();
+            installationDirectory = CheckInstallationPath();
+            WriteToLogWindow(txtLog, "Waiting for JMeter script...");
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            // Create OpenFileDialog 
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog
+            {
+                // Set filter for file extension and default file extension 
+                DefaultExt = ".jmx",
+                Filter = "JMeter scripts (*.jmx)|*.jmx"
+            };
+
+            // Display OpenFileDialog by calling ShowDialog method 
+            bool? result = dlg.ShowDialog();
+
+            // Get the selected file name and display in a TextBox 
+            if (result == true)
+            {
+                // Open document 
+                scriptFilename = System.IO.Path.GetFileNameWithoutExtension(dlg.FileName);
+                scriptFullPath = dlg.FileName;
+                scriptDirectoryPath = System.IO.Path.GetDirectoryName(dlg.FileName);
+                labelScriptName.Content = scriptFilename;
+                labelScriptName.Foreground = Brushes.ForestGreen;
+                txtScriptPath.Text = scriptFullPath;
+                FileOk = true;
+            }
+        }
+
+        private void btnRun_Click(object sender, RoutedEventArgs e)
+        {
+            if (FileOk)
+            {
+                txtLog.Text = string.Empty;
+                WriteToLogWindow(txtLog, "Script '" + scriptFilename + "' loaded!\r\n\r\n" +
+                    "Script started at: " + DateTime.Now.ToString("dd/MM/yyyy HH:mm:ss") + "\r\n\r\n" +
+                    "Please check JMeter console window for further test progress and results");
+                RunScript();
+            }
+            else
+            {
+                labelScriptName.Foreground = Brushes.Red;
+                labelScriptName.Content = "Please choose a JMeter script!";
+            }
+        }
+
+        private void RunScript()
+        {
+            string testCommandLine = "/K jmeter -n -t " + scriptFullPath;
+            string timestamp = DateTime.Now.Ticks.ToString();
+            string testResultsFolder = scriptFilename + "_" + timestamp;
+
+
+            if (checkboxGenerateCSV.IsChecked ?? false)
+            {
+                testCommandLine += " -l " + scriptDirectoryPath + System.IO.Path.DirectorySeparatorChar + testResultsFolder
+                    + System.IO.Path.DirectorySeparatorChar + testResultsFolder + ".csv";
+            }
+
+            if (checkboxGenerateHTML.IsChecked ?? false)
+            {
+                testCommandLine += " -e -o " + scriptDirectoryPath + System.IO.Path.DirectorySeparatorChar + testResultsFolder;
+            }
+
+            var process = new Process();
+            var startInfo = new ProcessStartInfo
+            {
+                WorkingDirectory = @installationDirectory,
+                WindowStyle = ProcessWindowStyle.Normal,
+                FileName = "cmd.exe",
+                Arguments = testCommandLine,
+            };
+
+            process.StartInfo = startInfo;
+            process.Start();
+        }
+
+        private void TxtScriptPath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+    }
+}
